e91bdc54d96b6e10e38b8a805fc1a51e95a2f410
siv2r <siv2ram@gmail.com>
2026-01-22 16:17:45 +0530

    python: remove broken secp256k1lab subtree

--- update-secp256k1lab.sh
+++ update-secp256k1lab.sh
@@ -1,2 +1,9 @@
+#!/bin/sh
+
+# Update the secp256k1lab git subtree to the latest upstream version
+#
+# The secp256k1lab library is vendored as a git subtree at python/secp256k1lab/
+# This script updates it to the latest version from the upstream repository.
+
 git fetch https://github.com/secp256k1lab/secp256k1lab.git master
 git subtree pull --prefix=python/secp256k1lab https://github.com/secp256k1lab/secp256k1lab.git master --squash
